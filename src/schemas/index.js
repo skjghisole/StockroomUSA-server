32d20eebed3c37f65ef434bca481d7444a228283
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-10-05 22:18:45 -0400

    implemented database to the server

---
+++ src/schemas/index.js
@@ -0,0 +1,77 @@
+import {
+	GraphQLObjectType,
+	GraphQLSchema,
+	GraphQLID,
+	GraphQLInt,
+	GraphQLString,
+	GraphQLList
+} from 'graphql'
+
+let db = {
+	products: []
+}
+
+const ProductType = new GraphQLObjectType({
+	name: 'Product',
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		quantity: { type: GraphQLInt }
+	})
+})
+
+
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		product: {
+			type: ProductType,
+			args: {
+				id: { type: GraphQLID }
+			},
+			resolve(parent, args) {
+				return db.products.find(x => x.id == args.id)
+			}
+		},
+		products: {
+			type: new GraphQLList(ProductType),
+			resolve(parent, args) {
+				return db.products
+			}
+		}
+	}
+})
+
+const Mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		addProduct: {
+			type: ProductType,
+			args: {
+				name: { type: GraphQLString },
+				quantity: { type: GraphQLInt }
+			},
+			resolve(parent, { name, quantity }) {
+				let product = { name, quantity, id: db.products.length }
+				db.products.push(product)
+				return product
+			}
+		},
+		removeProduct: {
+			type: ProductType,
+			args: {
+				id: { type: GraphQLID },
+			},
+			resolve(parent, { id }) {
+				const index = db.products.findIndex(product => product.id == id)
+				const product = db.products.splice(index, 1)
+				return product[0]
+			}
+		}
+	}
+})
+
+export default new GraphQLSchema({
+	query: RootQuery,
+	mutation: Mutation
+})
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,41 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import graphqlHTTP from 'express-graphql'
+
+import schema from '../schemas'
 
 const app = express()
+
+const {
+	DB_USER_USERNAME,
+	DB_USER_PASSWORD,
+	DB_PROTOCOL,
+	DB_PORT,
+	DB_HOST,
+	NODE_ENV
+} = process.env
+
+const hostedEnvs = ['staging', 'production', 'ci']
+let mongoURI;
+
+console.log(`Env is ${NODE_ENV}`)
+
+if (!hostedEnvs.some(env => env === NODE_ENV)) {
+	mongoURI = `mongodb://${DB_HOST}:${DB_PORT}/`
+} else {
+	mongoURI =	`${DB_PROTOCOL}://${DB_USER_USERNAME}:${DB_USER_PASSWORD}@cluster0-bnvga.mongodb.net/admin?retryWrites=true&w=majority`
+}
+
+mongoose.connect(mongoURI)
+mongoose.connection.once('open', () => {
+	console.log('DB connected')
+})
+
+
+app.use('/graphql', graphqlHTTP({
+	graphiql: true,
+	schema
+}))
 const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log(`Started on http://localhost:${port}`))
