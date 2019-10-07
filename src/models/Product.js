7fdae25ee60f771056a91258d5a58cd23a4a4e80
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-10-06 23:40:50 -0400

    added categoryIds and brandIds for product schema

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,8 +5,8 @@ const { Schema, model, Types: { ObjectId } } = mongoose
 const ProductSchema = new Schema({
 	name: String,
 	quantity: Number,
-	categoryId: ObjectId,
-	brandId: ObjectId
+	categoryIds: [ObjectId],
+	brandIds: [ObjectId]
 })
 
 export default model('Product', ProductSchema)
