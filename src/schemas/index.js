d111dd4c0ce38d681142c3b1df12b79b79df25bb
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-10-08 16:14:25 -0400

    debug 9

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,11 +1,33 @@
 import {
-	GraphQLSchema
+	GraphQLSchema,
+	GraphQLObjectType,
+	GraphQLList
 } from 'graphql'
+
+import {
+	UserType
+} from './Types/User'
+
+import {
+	User
+} from '../models'
 
 // import query from './Queries/Root'
 import mutation from './Mutations/Root'
 
+const QueryType = new GraphQLObjectType({
+	name: 'QueryType',
+	fields: {
+		users: {
+			type: new GraphQLList(UserType),
+			resolve() {
+				return User.find({})
+			}
+		}
+	}
+})
+
 export default new GraphQLSchema({
-	// query,
+	query: QueryType,
 	mutation
 })
