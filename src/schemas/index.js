5f58fc10c74d8095ce1d3f88ebe1b78dd0d9bf79
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-10-08 16:16:25 -0400

    returned query

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,33 +1,33 @@
 import {
 	GraphQLSchema,
-	GraphQLObjectType,
-	GraphQLList
+	// GraphQLObjectType,
+	// GraphQLList
 } from 'graphql'
 
-import {
-	UserType
-} from './Types/User'
+// import {
+// 	UserType
+// } from './Types/User'
 
-import {
-	User
-} from '../models'
+// import {
+// 	User
+// } from '../models'
 
-// import query from './Queries/Root'
+import query from './Queries/Root'
 import mutation from './Mutations/Root'
 
-const QueryType = new GraphQLObjectType({
-	name: 'QueryType',
-	fields: {
-		users: {
-			type: new GraphQLList(UserType),
-			resolve() {
-				return User.find({})
-			}
-		}
-	}
-})
+// const QueryType = new GraphQLObjectType({
+// 	name: 'QueryType',
+// 	fields: {
+// 		users: {
+// 			type: new GraphQLList(UserType),
+// 			resolve() {
+// 				return User.find({})
+// 			}
+// 		}
+// 	}
+// })
 
 export default new GraphQLSchema({
-	query: QueryType,
+	query,
 	mutation
 })
