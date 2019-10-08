3a9534cded1998d67a44d348db10c7a4dc598699
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-10-08 16:37:06 -0400

    debug 13

---
+++ src/schemas/queries/Root.js
@@ -0,0 +1,42 @@
+import {
+	GraphQLObjectType,
+	// GraphQLList
+} from 'graphql'
+
+// import {
+// 	UserType
+// } from '../Types'
+
+// import {
+// 	User
+// } from '../../models'
+
+
+import {
+	BrandQueries,
+	CategoryQueries,
+	ProductQueries,
+	UserQueries
+} from './'
+
+const fields = {
+	...BrandQueries,
+	...CategoryQueries,
+	...ProductQueries,
+	...UserQueries
+}
+
+const QueryType = new GraphQLObjectType({
+	name: 'QueryType',
+	fields,
+	// fields: {
+	// 	users: {
+	// 		type: new GraphQLList(UserType),
+	// 		resolve() {
+	// 			return User.find({})
+	// 		}
+	// 	}
+	// }
+})
+
+export default QueryType
