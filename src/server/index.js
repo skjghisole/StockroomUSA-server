50389641ce55fee70bb1c656c88c669a729b1ace
Stephen Karl Jeoffrey Hisole <skjghisole@yahoo.com>
2019-10-08 15:38:06 -0400

    checking

--- src/server/index.js
+++ src/server/index.js
@@ -1,45 +1,45 @@
 // import '@babel/polyfill'
 import express from 'express'
-import mongoose from 'mongoose'
-import graphqlHTTP from 'express-graphql'
-import schema from '../schemas'
-import cors from 'cors'
+// import mongoose from 'mongoose'
+// import graphqlHTTP from 'express-graphql'
+// import schema from '../schemas'
+// import cors from 'cors'
 
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 
 const {
-	DB_USER_USERNAME,
-	DB_USER_PASSWORD,
-	DB_PROTOCOL,
-	DB_PORT,
-	DB_HOST,
-	DB_NAME,
-	NODE_ENV,
+// 	DB_USER_USERNAME,
+// 	DB_USER_PASSWORD,
+// 	DB_PROTOCOL,
+// 	DB_PORT,
+// 	DB_HOST,
+// 	DB_NAME,
+// 	NODE_ENV,
 	PORT
 } = process.env
 
-const hostedEnvs = ['staging', 'production', 'ci']
-let mongoURI;
+// const hostedEnvs = ['staging', 'production', 'ci']
+// let mongoURI;
 
-console.log(`NODE_ENV -----------> ${NODE_ENV} <-----------`)
+// console.log(`NODE_ENV -----------> ${NODE_ENV} <-----------`)
 
-if (!hostedEnvs.some(env => env === NODE_ENV)) {
-	mongoURI = `${DB_PROTOCOL}://${DB_HOST}:${DB_PORT}/${DB_NAME}`
-} else {
-	mongoURI = `${DB_PROTOCOL}://${DB_USER_USERNAME}:${DB_USER_PASSWORD}@cluster0-bnvga.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
-}
+// if (!hostedEnvs.some(env => env === NODE_ENV)) {
+// 	mongoURI = `${DB_PROTOCOL}://${DB_HOST}:${DB_PORT}/${DB_NAME}`
+// } else {
+// 	mongoURI = `${DB_PROTOCOL}://${DB_USER_USERNAME}:${DB_USER_PASSWORD}@cluster0-bnvga.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+// }
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.connection.once('open', () => {
-	console.log('DB connected')
-})
+// mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connection.once('open', () => {
+// 	console.log('DB connected')
+// })
 
-app.use('/graphql', graphqlHTTP({
-	graphiql: true,
-	schema
-}))
+// app.use('/graphql', graphqlHTTP({
+// 	graphiql: true,
+// 	schema
+// }))
 
 const port = PORT || 4545
 
