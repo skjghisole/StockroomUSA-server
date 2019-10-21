f244b11b524207fc03b45c9d9149280236497b59
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-10-21 16:55:41 -0400

    added category subscription

---
+++ src/schemas/Subscriptions/Category/index.js
@@ -0,0 +1,20 @@
+import { withFilter } from 'graphql-subscriptions'
+import { CategoryType } from '../../Types'
+
+import { CATEGORY_ADDED } from './constants' 
+
+import { pubsub } from '../../../utils'
+
+const CategorySubscription = {
+	categoryAdded: {
+		type: CategoryType,
+		subscribe: withFilter(() => pubsub.asyncIterator(CATEGORY_ADDED), (payload, variables) => {
+			return true
+		}),
+		resolve(payload) {
+			return payload
+		}		
+	}
+}
+
+export default CategorySubscription
