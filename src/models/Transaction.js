29d34a14e2471ef4cc94612af8ef62f43a0ebace
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-22 23:16:55 -0500

    feat: added transaction model
    added transaction model

---
+++ src/models/Transaction.js
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose'
+
+const { Schema, model, Types: { ObjectId } } = mongoose
+
+const ItemSchema = new Schema({
+	productId: {
+		type: ObjectId
+	},
+	quantity: {
+		type: Number
+	},
+	size: {
+		type: String
+	},
+	color: {
+		type: String
+	}
+})
+
+const TransactionSchema = new Schema({
+	recipient: {
+		firstName: {
+			type: String,
+			default: ''
+		},
+		lastName: {
+			type: String,
+			default: ''
+		},
+		email: {
+			type: String,
+			default: ''
+		}
+	},
+	items: [ItemSchema],
+	productIds: {
+		type: [ObjectId]
+	},
+	shippingAddress: {
+		mainAddress: {
+			type: String,
+			default: ''
+		},
+		secondaryAddress: {
+			type: String,
+			default: ''
+		},
+		street: {
+			type: String,
+			default: ''
+		},
+		city: {
+			type: String,
+			default: ''
+		},
+		country: {
+			type: String,
+			default: 'Philippines'
+		}
+	},
+	createdAt: Date,
+	updatedAt: Date
+}, {
+	strict: true,
+	timestamps: {
+		createdAt: 'createdAt',
+		updatedAt: 'updatedAt'
+	}
+})
+
+export default model('Transaction', TransactionSchema)
