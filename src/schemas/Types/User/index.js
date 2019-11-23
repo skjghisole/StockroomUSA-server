f4ec4c4874e600ce3842498e240883be559258d8
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-22 23:21:17 -0500

    improvement: improved the user root type schema
    improved the user root type schema

--- src/schemas/Types/User/index.js
+++ src/schemas/Types/User/index.js
@@ -1,11 +1,14 @@
 import UserType from './User'
-import CredentialType from './Credential'
+import { CredentialType, CredentialInputType } from './Credential'
 import RoleType from './Role'
 import TokenType from './Token'
+import { AddressType } from './Address'
 
 export {
 	UserType,
 	CredentialType,
+	CredentialInputType,
 	RoleType,
-	TokenType
+	TokenType,
+	AddressType
 }
