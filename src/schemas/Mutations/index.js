3f48bc818f4abb827fbd117daebaec313cb2b100
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-23 23:26:30 -0500

    improvement: added transaction mutation to the root mutation schema
    added transaction mutation to the root mutation schema

--- src/schemas/Mutations/index.js
+++ src/schemas/Mutations/index.js
@@ -2,10 +2,12 @@ import BrandMutations from './Brand'
 import CategoryMutations from './Category'
 import ProductMutations from './Product'
 import UserMutations from './User'
+import TransactionMutations from './Transaction'
 
 export {
 	BrandMutations,
 	CategoryMutations,
 	ProductMutations,
-	UserMutations
+	UserMutations,
+	TransactionMutations
 }
