aedb2917a504c4bb49e44488e1661a7b5025007e
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-23 23:21:45 -0500

    improvement: added transaction schema on models root
    added transaction schema on models root

--- src/models/index.js
+++ src/models/index.js
@@ -2,10 +2,12 @@ import Product from './Product'
 import Category from './Category'
 import Brand from './Brand'
 import User from './User'
+import Transaction from './Transaction'
 
 export {
 	Product,
 	Category,
 	Brand,
-	User
+	User,
+	Transaction
 }
