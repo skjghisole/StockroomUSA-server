c020e7b87d5c8dfc9862e61b0b51bb73e23660c1
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-23 23:29:26 -0500

    improvement: refactored the transaction index schema
    refactored the transaction index schema

--- src/schemas/Types/Transaction/index.js
+++ src/schemas/Types/Transaction/index.js
@@ -1,30 +1,8 @@
-import { GraphQLObjectType, GraphQLList } from 'graphql';
+import TransactionType from './Transaction';
+import { ItemDetailsInputType, ItemDetailsType } from './ItemDetails';
 
-import { AddressType, ProductType, CredentialType } from '../';
-
-import { Product } from '../../../models/Product';
-
-const TransactionType = new GraphQLObjectType({
-	name: 'Transaction',
-	fields: () => ({
-		shippingAddres: {
-			type: AddressType
-		},
-		items: {
-			type: GraphQLList(ProductType),
-			async resolve({ items }) {
-				const ids = items.reduce((acc, curr) => acc.concat(curr.productId), [])
-				const products = Product.find({ _id: { $in: ids }})
-				return products.reduce((acc, curr) => {
-					const itemDetails = items.find(({productId}) => curr._id == productId)
-					return acc.concat(Object.assign({}, curr, { itemDetails }))
-				}, [])
-			}
-		},
-		recipient: {
-			type: CredentialType
-		}
-	})
-})
-
-export default TransactionType;
+export {
+	TransactionType,
+	ItemDetailsType,
+	ItemDetailsInputType
+}
