aa2d9ecbb8839f5b393acad324c04073593e776b
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-23 23:27:50 -0500

    improvement: added transaction query to the index queries to be imported from other components
    added transaction query to the index queries to be imported from other components

--- src/schemas/Queries/index.js
+++ src/schemas/Queries/index.js
@@ -2,10 +2,12 @@ import BrandQueries from './Brand'
 import CategoryQueries from './Category'
 import ProductQueries from './Product'
 import UserQueries from './User'
+import TransactionQueries from './Transaction'
 
 export {
 	BrandQueries,
 	CategoryQueries,
 	ProductQueries,
-	UserQueries
+	UserQueries,
+	TransactionQueries
 }
