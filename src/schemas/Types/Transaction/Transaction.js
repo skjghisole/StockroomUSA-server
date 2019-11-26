00ca31319ff46cd29bb93e394bd04f67d4284ec2
Stephen Karl Jeoffrey  Hisole <skjghisole@yahoo.com>
2019-11-25 21:47:57 -0500

    improvement: working items query for the transaction schema type
    working items query for the transaction schema type

--- src/schemas/Types/Transaction/Transaction.js
+++ src/schemas/Types/Transaction/Transaction.js
@@ -2,7 +2,7 @@ import { GraphQLObjectType, GraphQLList } from 'graphql';
 
 import { AddressType, ProductType, CredentialType, ItemDetailsType } from '../';
 
-import { Product } from '../../../models/Product';
+import { Product } from '../../../models/';
 
 const TransactionType = new GraphQLObjectType({
 	name: 'Transaction',
@@ -17,12 +17,11 @@ const TransactionType = new GraphQLObjectType({
 			type: GraphQLList(ProductType),
 			async resolve({ itemDetails }) {
 				const ids = itemDetails.reduce((acc, curr) => acc.concat(curr.productId), [])
-				console.log(ids)
 				const products = await Product.find({ _id: { $in: ids }})
-				console.log(products)
-				return products.reduce((acc, curr) => {
-					const itemDetails = itemDetails.find(({ productId }) => curr._id == productId)
-					return acc.concat(Object.assign({}, curr, { itemDetails }))
+				return itemDetails.reduce((acc, curr) => {
+					const { images, name, sizes, quantity, categoryIds, brandIds, description, price, _id } = products.find(({ _id }) => _id.toString() === curr.productId.toString())
+					const current = { itemDetails, id: _id, images, name, sizes, quantity, categoryIds, brandIds, description, price }
+					return acc.concat(current)
 				}, [])
 			}
 		},
